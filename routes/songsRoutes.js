f50669e09f61038ca16eaea6daeaaf7eca766ecd
melnard <dejesusmelnard@gmail.com>
2025-06-11 12:19:19 +0800

    feat(song book) make songRoutes a public route

--- routes/songsRoutes.js
+++ routes/songsRoutes.js
@@ -5,7 +5,7 @@ const songController = require('../controllers/songController');
 const authController = require('../controllers/authController');
 
 // Protect all routes after this middleware
-router.use(authController.protect);
+// router.use(authController.protect);
 
 router.route('/').get(songController.songs);
 
